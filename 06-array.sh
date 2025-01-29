274cc77231d016d8c1c114cdbc4f8b0e47ee8fb9
Ram-eng01 <rammohan63025@gmail.com>
2025-01-29 11:02:26 +0530

    some-message

---
+++ 06-array.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MOVIES=("kgf" "rrr" "kgf-2")
+
+#size of above array is 3
+# index are 0,1,2
+
+# ist always start with 0
+
+echo "first movie is: ${MOVIES[0]}"
+
+echo "second movie is: ${MOVIES[1]}"
+
+echo "all movies are: ${MOVIES[@]}"
