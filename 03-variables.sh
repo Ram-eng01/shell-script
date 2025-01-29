2fa36eaa9f129f8aa0650299a442116decad32fb
Ram-eng01 <rammohan63025@gmail.com>
2025-01-29 10:39:46 +0530

    some-message

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Rammohan   # no space between = and value
+PERSON2=Suresh
+
+echo "$PERSON1:: Helo suresh,How are you?"
+echo "$PERSON2:: Hi Ramesh, I am fine. How are you?"
+echo "$PERSON1: I am fine too. how is your work?"
+echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
