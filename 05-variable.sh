e52855b5d7d3bd86587d94dee6b0fcfd5608694e
Ram-eng01 <rammohan63025@gmail.com>
2025-01-29 10:52:00 +0530

    some-message

---
+++ 05-variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+echo "please enter username:: "
+
+read USERNAME  #here USERNAME is variable
+
+echo "Pease enter Password:: "
+
+read PASSWORD
+
+
+echo "username is: $USERNAME, password is: $PASSWORD"
