4e9dda9c3218ae128a8823cb887bab353d0015d5
Ram-eng01 <rammohan63025@gmail.com>
2025-02-05 17:25:11 +0530

    shell-project

--- 18-delete-logs.sh
+++ 18-delete-logs.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-SOURCE_DIR=/tmp/app-logs
+SOURCE_DIRECTORY=/tmp/app-logs
 
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-
-if [ -d $SOURCE_DIR]
+if [ -d $SOURCE_DIRECTORY ]
 then
-    echo -e "$G source directory exists $N"
+    echo -e "$G Source directory exists $N"
 else
-    echo -e "$R please make sure $SOURCE_DIR exits $N" 
+    echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
     exit 1
 fi
