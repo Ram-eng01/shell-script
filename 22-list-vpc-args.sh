f3246eb0b301a81048f964ef2b07a9a6819568ab
Ram-eng01 <rammohan63025@gmail.com>
2026-02-02 00:14:48 +0530

    added shell files

--- 22-list-vpc-args.sh
+++ 22-list-vpc-args.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#REGION=$1
-#aws ec2 describe-vpcs --region $REGION | jq ".Vpcs[].VpcId"
+# REGION=$1
+# aws ec2 describe-vpcs --region $REGION | jq ".Vpcs[].VpcId"
 
 
 
 
-REGIONS="$*"
-for REGION in $REGIONS; do
-    aws ec2 describe-vpcs --region $REGIONS | jq ".Vpcs[].VpcId"
+for REGION in "$@"; do
+    aws ec2 describe-vpcs --region "$REGION" \
+        | jq -r '.Vpcs[].VpcId'
 done
