1a66702a9db62c71c3975c5e5834d4834a643e86
Ram-eng01 <rammohan63025@gmail.com>
2026-02-02 01:15:12 +0530

    added shell files

--- 23-find-mem-utilization.sh
+++ 23-find-mem-utilization.sh
@@ -6,13 +6,13 @@ TOTAL_AVAIL=$(free -m | grep -i "mem" | awk -F " " '{print $7}')
 USED_MEMORY=$(expr $TOTAL_MEM - $TOTAL_AVAIL)
 echo "The total memory In the machine is ${TOTAL_MEM}MB and current Utilization is ${USED_MEMORY}MB"
 
-FREE_PERCENT=$(awk -v avail="$TOTAL_AVAIL" -v total="$TOTAL_MEM" \
-    'BEGIN { (avail/total)*100 }')
+X=$(echo "scale=2; $TOTAL_AVAIL / $TOTAL_MEM "| bc | tr -d ".")
 
-echo "The free memory percentage is ${FREE_PERCENT}%"
-CURRENT_UTL_PER=$(expr 100 - $FREE_PERCENT)
 
-if [ $FREE_PERCENT -le 10 ];
+echo "The free memory percentage is ${X}%"
+CURRENT_UTL_PER=$(expr 100 - $X)
+
+if [ $X -le 10 ];
 then  
    echo "Mmeory is utilizec more than 90%"
 else
