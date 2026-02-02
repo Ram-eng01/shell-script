78c203244f23097f0d2812fbecccd05904ef03be
Ram-eng01 <rammohan63025@gmail.com>
2026-02-03 01:41:53 +0530

    added shell files

---
+++ 27-user-automation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#check if the user already exists and create if not exists.
+#Generate random password and assign to the user
+#Expire the password forcing the user to reset it
+#make sure user has sudo persmission.
+
+USER_NAME=$1
+EXISTING_USER=$(cat /etc/passwd | grep -i -w ${USER_NAME} | cut -d ":" -f 1)
+if [ "${USER_NAME}" = "${EXISTING_USER}"];
+then
+    echo "The user ${USER_NAME} already exists. Try a different username.
+else
+   echo "lets create user ${USER_NAME}"
+fi
