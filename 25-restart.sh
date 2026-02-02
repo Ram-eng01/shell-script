e73c5ae989e2277d0d45f0499e5a271aa863a6f6
Ram-eng01 <rammohan63025@gmail.com>
2026-02-02 16:16:39 +0530

    shel-script

--- 25-restart.sh
+++ 25-restart.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-
-aws ec2 describe-vpcs --region $@ | jq ".Vpcs[].VpcId"
-
+if [ $# -gt 0 ];
+then
+    aws ec2 describe-vpcs --region $@ | jq ".Vpcs[].VpcId"
+else
+    echo "You have given $# parameters to this script, Please provide parameter at least 1 eg; us-east-1"
 echo $#
